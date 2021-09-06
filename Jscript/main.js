fd5769cd83e20d489ebd1d7fd982aafb9815ab3c
RobbinGIT <robbin.githimbo@student.moringaschool.com>
2021-09-06 20:39:07 +0300

    add the comments

--- Jscript/main.js
+++ Jscript/main.js
@@ -12,7 +12,7 @@ function output() {
   var birthday = new Date(mm + "/" + dd + "/" + yy);
   var dayOfTheWeek = birthday.getDay();
   
-   // using the formal below
+   // using the formular below
    
   var result = parseInt(cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7;
   
